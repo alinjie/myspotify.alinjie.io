72870923c9c0114d4566e21d102d05d6a29c6fcd
ali.njie <alinjie@Alis-iMac.local>
2020-11-12 23:11:03 +0100

    chore: tailwind init

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        "spotify-green": "#1DB954",
+        "spotify-black": "#191414"
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
