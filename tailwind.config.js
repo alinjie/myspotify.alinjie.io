d559d26e21e6ac0837ab9a3311227d2c2f1f8a47
ali.njie <alinjie@Alis-iMac.local>
2020-11-15 14:07:41 +0100

    perf: add Tailwind purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,9 @@ module.exports = {
     // removeDeprecatedGapUtilities: true,
     // purgeLayersByDefault: true,
   },
-  purge: [],
+  purge: [
+    "**/*.{ts,tsx}"
+  ],
   theme: {
     extend: {
       width: {
