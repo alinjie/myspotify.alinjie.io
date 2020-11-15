cc64504eb839bb06199e89edaaccf99815bcfd6d
ali.njie <alinjie@Alis-iMac.local>
2020-11-15 14:25:00 +0100

    chore(auth): add dynamic callback url

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -2,6 +2,7 @@ import cx from "classnames";
 import { signIn, useSession } from "next-auth/client";
 import { HTMLProps } from "react";
 import Header from "components/Header";
+import { useRouter } from "next/dist/client/router";
 
 export default function Layout({
   className,
@@ -10,8 +11,16 @@ export default function Layout({
 }: HTMLProps<HTMLDivElement>) {
   const [session, loading] = useSession();
 
+  if (typeof window === "undefined") return null;
+
+  const { host } = window.location;
+
   if (!session || new Date(session.expires) < new Date())
-    return <button onClick={() => signIn("spotify")}>Sign in</button>;
+    return (
+      <button onClick={() => signIn("spotify", { callbackUrl: host })}>
+        Sign in
+      </button>
+    );
 
   if (loading) return <h1>Loading...</h1>;
 
