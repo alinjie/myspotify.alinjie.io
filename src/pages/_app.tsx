1f9d272d806243b5ecdb986be0ed9f63c305b9e1
ali.njie <alinjie@Alis-iMac.local>
2020-11-23 20:16:35 +0100

    chore: add favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,15 @@
 import Layout from "components/Layout";
 import { AppProps } from "next/app";
 import { Provider } from "next-auth/client";
+import Head from "next/head";
 import "styles/index.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider session={pageProps.session}>
+      <Head>
+        <title>Spotify Profile</title>
+      </Head>
       <Layout>
         <Component {...pageProps} />
       </Layout>
